647942f225c731b31a9b6a567fa7efe2405eb915
Marc <marc.mendia@epitech.eu>
2024-09-18 17:36:51 +0200

    [FIX] remove randscreen

--- StopRandScreen.sh
+++ StopRandScreen.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
 
-jobs -p | while read -r job; do
-  echo "Terminating background job with PID: $job"
-
-  kill "$job"
-
-  sleep 5
-  if kill -0 "$job" 2>/dev/null; then
-    echo "Forcing termination of job with PID: $job"
-    kill -9 "$job"
-  fi
-done
-
-echo "All background tasks have been terminated."
+reboot
