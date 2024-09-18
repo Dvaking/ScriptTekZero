00a00536c210991caa3b47c547b7cc761651311a
Marc <marc.mendia@epitech.eu>
2024-09-18 14:28:33 +0200

    [ADD] randScreen.sh

---
+++ randScreen.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+while true
+do
+  random_number=$((1 + RANDOM % 4))
+  if [random_number === 1]
+  then
+    echo "1"
+    xrandr left
+  elif [random_number === 2]
+  then
+    echo "2"
+    xrandr right
+  elif [random_number === 3]
+    echo "3"
+    xrandr inverted
+  else
+    echo "4"
+    xrandr normal
+  fi
+  sleep 1m
+done
