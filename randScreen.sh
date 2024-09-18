da2422e868d7f052cf10269405cd30b5ee0c7538
Marc <marc.mendia@epitech.eu>
2024-09-18 15:14:04 +0200

    [FIX] try with randmo gnome

--- randScreen.sh
+++ randScreen.sh
@@ -2,7 +2,7 @@
 session=$XDG_SESSION_TYPE
 folder=$(mktemp -d)
 
-curl https://github.com/Dvaking/ScriptTekZero/raw/main/wlr-randr -o $folder/
+curl https://raw.githubusercontent.com/Dvaking/ScriptTekZero/main/gnome-randr.py -o $folder/gnome-randr.py
 
 
 if [ "$session" == "x11" ]; then
@@ -30,16 +30,16 @@ do
     random_number=$((1 + RANDOM % 4))
     if [ "$random_number" == "1" ]; then
         echo "1"
-        $folder/wlr-randr --output eDP-1 --transform 270
+        $folder/gnome-randr.py --output --auto --rotate left
         elif [ "$random_number" == "2" ]; then
         echo "2"
-        $folder/wlr-randr --output eDP-1 --transform 90
+        $folder/gnome-randr.py --output --auto --rotate right
         elif [ "$random_number" == "3" ]; then
         echo "3"
-        $folder/wlr-randr --output eDP-1 --transform 180
+        $folder/gnome-randr.py --output --auto --rotate inverted
         elif [ "$random_number" == "4" ]; then
         echo "4"
-        $folder/wlr-randr --output eDP-1 --transform normal
+        $folder/gnome-randr.py --output --auto --rotate normal
     fi
     sleep 30s
 done
